cb76cb19a6bb5940d4959895b7db3d1631da1dc1
Luke Klinker <lklinker1@gmail.com>
2015-09-17 09:23:28 -0500

    fixes

--- src/classes/AccountDropdownBuilder.php
+++ src/classes/AccountDropdownBuilder.php
@@ -3,21 +3,6 @@
 class AccountDropdownBuilder {
 
     public static function buildDropdown($db, $session) {
-        switch($session['user']['user_type_id']) {
-            case 1:
-                $type = "patient";
-                break;
-            case 2:
-                $type = "doctor";
-                break;
-            case 3:
-                $type = "nurse";
-                break;
-            case 4:
-                $type = "administrator";
-                break;
-        }
-
         $query = "
                 SELECT picture_url
                 FROM user
@@ -25,7 +10,7 @@ class AccountDropdownBuilder {
                     _id = :id
                 ";
         $query_params = array(
-            ':id' => $session['user']['id']
+            ':id' => $session['user']['_id']
         );
 
         try {
@@ -44,12 +29,10 @@ class AccountDropdownBuilder {
         echo "<a class=\"dropdown-toggle\" href=\"#\" data-toggle=\"dropdown\">Account  <strong class=\"caret\"></strong></a>";
         echo "<div class=\"dropdown-menu\" style=\"padding: 15px; padding-bottom: 0px;\">";
         echo "<a href=\"" . $profileLink . "\">";
-        echo "<div class=\"center_image_dropdown\"><img src=\"" . $pictureUrl . "\" /></div>";
+        echo "<div class=\"center_image_dropdown\"><img src='" . $pictureUrl . "' /></div>";
         echo "</a>";
         echo "<b>" . $session['user']['first_name'] . " " . $session['user']['last_name'] . "</b><br/><br/>";
         echo "<a href=\"change_password.php\">Change Password</a><br/>";
-        echo "<a href=\"email_preferences.php\">Email Preferences</a><br/>";
-        echo "<a href=\"" . $type . "_info.php\">Update information</a><br/>";
         echo "<a href=\"delete_account.php\">Delete Account</a><br/><br/>";
         echo "<a href=\"upload_photo.php\">Upload Photo</a><br/><br/>";
         echo "</div>";
