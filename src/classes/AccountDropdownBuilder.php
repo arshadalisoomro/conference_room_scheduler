9af2250a01d6021147d028cbc8990fa207e91ea9
Luke Klinker <lklinker1@gmail.com>
2015-09-17 17:16:50 -0500

    move the link below the profile picture

--- src/classes/AccountDropdownBuilder.php
+++ src/classes/AccountDropdownBuilder.php
@@ -25,11 +25,11 @@ class AccountDropdownBuilder {
 
         $profileLink = "http://dbsystems-engproject.rhcloud.com/src/user_page.php?id=" . $session['user']['_id'];
 
-        echo "<a class=\"mdl-navigation__link\" href=\"home.php\">Home</a>";
         echo "<a class=\"padding_left_class\" href=\"" . $profileLink . "\">";
         echo "<div class=\"center_image_dropdown\"><img src='" . $pictureUrl . "' /></div>";
         echo "</a>";
         echo "<b class=\"padding_left_class\">" . $session['user']['first_name'] . " " . $session['user']['last_name'] . "</b>";
+        echo "<a class=\"mdl-navigation__link\" href=\"home.php\">Home</a>";
         echo "<a class=\"mdl-navigation__link\" href=\"change_password.php\">Change Password</a>";
         echo "<a class=\"mdl-navigation__link\" href=\"upload_photo.php\">Upload Photo</a>";
         echo "<a class=\"mdl-navigation__link\" href=\"logout.php\">Log Out</a>";
