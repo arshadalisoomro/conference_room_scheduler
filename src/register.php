4f59ccf849dce9eb51cd78e8bdaa486bebd96dbb
Luke Klinker <lklinker1@gmail.com>
2015-09-17 16:14:59 -0500

    change some text

--- src/register.php
+++ src/register.php
@@ -44,7 +44,7 @@
     <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
         <header class="mdl-layout__header mdl-layout__header--waterfall">
             <div class="mdl-layout__header-row">
-                <span class="mdl-layout-title">Reset Password</span>
+                <span class="mdl-layout-title">Register as Admin</span>
             </div>
         </header>
         <div class="mdl-layout__drawer">
