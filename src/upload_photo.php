378d002b508803f71696fecbe223d8171dcc7145
Luke Klinker <lklinker1@gmail.com>
2015-09-17 17:11:18 -0500

    revert button because it didn't work

--- src/upload_photo.php
+++ src/upload_photo.php
@@ -92,15 +92,8 @@
                     ?>   
                     <p>Please select a file by clicking the 'Choose File' button and press 'Upload' to start uploading your file.</p>
                     <form action="" method="post" enctype="multipart/form-data" name="form1" id="form1">
-                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--file">
-                            <input class="mdl-textfield__input" placeholder="File" type="text" id="uploadFile" readonly/>
-                            <div class="mdl-button mdl-button--primary mdl-button--icon mdl-button--file">
-                              <i class="material-icons">attach_file</i><input name="theFile" type="file" id="uploadBtn">
-                            </div>
-                        </div>
-                        <button name="Submit" type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
-                          Upload
-                        </button>
+                      <input name="theFile" type="file" />
+                      <input name="Submit" type="submit" value="Upload">
                     </form>
                 </div>
               </div>
