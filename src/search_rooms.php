15771532620e744e41c120ec2f08f63d9007caf3
Luke Klinker <lklinker1@gmail.com>
2015-10-13 10:40:06 -0500

    added files

---
+++ src/search_rooms.php
@@ -0,0 +1,52 @@
+<?php
+
+    include_once('../AutoLoader.php');
+    AutoLoader::registerDirectory('../src/classes');
+
+    require("config.php");
+
+    $user = $_SESSION['user'];
+    $roomBuilder = new roomBuilder($db, $user);
+
+    if(empty($_SESSION['user'])) {
+        header("Location: ../index.php");
+        die("Redirecting to index.php"); 
+    }
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Conference Room</title>
+    <meta name="description" content="Conference room management system for Database Systems">
+    <meta name="author" content="Team 6">
+
+    <link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.2/material.indigo-pink.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="../main.css" rel="stylesheet" type="text/css">
+</head>
+
+
+<body class="mdl-demo mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <header class="mdl-layout__header mdl-layout__header--waterfall">
+            <div class="mdl-layout__header-row">
+                <span class="mdl-layout-title">Find a Room</span>
+            </div>
+        </header>
+        <div class="mdl-layout__drawer">
+            <span class="mdl-layout-title">Scheduler</span>
+            <nav class="mdl-navigation">
+                <?php AccountDropdownBuilder::buildDropdown($db, $_SESSION) ?>
+            </nav>
+        </div>
+        <main class="mdl-layout__content">
+            <br/>
+            <?php $roomBuilder->buildCards(); ?>
+        </main>
+    </div>
+    
+    <script src="https://storage.googleapis.com/code.getmdl.io/1.0.2/material.min.js"></script>
+</body>
+</html>
