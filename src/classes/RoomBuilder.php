7dda2f9d2aae6e92a5f133253bd1bf77bf591874
Luke Klinker <lklinker1@gmail.com>
2015-10-13 11:00:28 -0500

    some testing

--- src/classes/RoomBuilder.php
+++ src/classes/RoomBuilder.php
@@ -2,26 +2,6 @@
 
 class RoomBuilder {
     static function buildCards($db) {
-        $query = "SELECT room_number FROM room";
-
-        // execute the statement
-        try {
-            $stmt = $db->prepare($query);
-            $result = $stmt->execute();
-
-            // loop through, adding the all the rooms in a seperate card
-            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                echo '<section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">';
-                echo '  <div class="mdl-card mdl-cell mdl-cell--12-col">';
-                echo '      <div class="mdl-card__supporting-text">';
-                echo '          <h3>Room Number:' . $row['room_number'] . '</h3>'
-                echo '      </div>';
-                echo '  </div>';
-                echo '</section>';
-                echo '</br>';
-            }
-        } catch(Exception $e) {
-            echo $e->getMessage();
-        }
+        
     }
 }
