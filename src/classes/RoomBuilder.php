a8447f20973a3844c5ea5c55b6271424020d1dd9
Luke Klinker <lklinker1@gmail.com>
2015-10-13 11:17:46 -0500

    new line on html/php echos

--- src/classes/RoomBuilder.php
+++ src/classes/RoomBuilder.php
@@ -11,15 +11,15 @@ class RoomBuilder {
 
             // loop through, adding the all the rooms in a seperate card
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                echo '<section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">';
-                echo '  <div class="mdl-card mdl-cell mdl-cell--12-col">';
-                echo '      <div class="mdl-card__supporting-text">';
-                echo '          <h3>Room Number: ' . $row['room_number'] . '</h3>';
-                echo '          <p>Resources: ' . $this->getResourcesString($row['_id']) . '</p>';
-                echo '      </div>';
-                echo '  </div>';
-                echo '</section>';
-                echo '</br>';
+                echo '<section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">' . "\r\n";
+                echo '  <div class="mdl-card mdl-cell mdl-cell--12-col">' . "\r\n";
+                echo '      <div class="mdl-card__supporting-text">' . "\r\n";
+                echo '          <h3>Room Number: ' . $row['room_number'] . '</h3>' . "\r\n";
+                echo '          <p>Resources: ' . $this->getResourcesString($row['_id']) . '</p>' . "\r\n";
+                echo '      </div>' . "\r\n";
+                echo '  </div>' . "\r\n";
+                echo '</section>' . "\r\n";
+                echo '</br>' . "\r\n";
             }
         } catch(Exception $e) {
             echo $e->getMessage();
