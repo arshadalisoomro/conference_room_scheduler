1075ddeb12d81c9f174e9c9721b5408a11475349
Luke Klinker <lklinker1@gmail.com>
2015-10-20 09:42:52 -0500

    insert the reservations to the database

--- src/schedule_reservation.php
+++ src/schedule_reservation.php
@@ -1,6 +1,27 @@
 <?php 
 
-echo $_GET['room_id'] . "<br/>";
-echo $_GET['date'] . "<br/>";
-echo $_GET['time_slot'] . "<br/>";
-echo $_GET['user_id'];
+include_once('../AutoLoader.php');
+AutoLoader::registerDirectory('../src/classes');
+
+require("config.php");
+
+$insertStatement = "INSERT INTO reservation 
+					(`user_id`, `conference_room_id`, `time_slot_id`, `date`) 
+					VALUES (:user_id,:conference_room_id,:time_slot_id,:date_val)";
+
+$insertParams = array(
+            ':user_id' => $_GET['user_id'],
+            ':conference_room_id' => $_GET['room_id'],
+            ':time_slot_id' => $_GET['time_slot'],
+            ':date_val' => $_GET['date']
+        );
+
+try {
+    $stmt = $db->prepare($insertStatement);
+    $result = $stmt->execute($insertParams);
+
+	header("Location: home.php");
+	die("Redirecting to home.php");
+} catch(PDOException $ex) {
+    echo "exception: " . $ex->getMessage();
+}
