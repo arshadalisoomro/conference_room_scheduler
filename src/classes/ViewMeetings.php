e575fd0ac3c50c40471e40fed971b38f02c1bb56
Luke Klinker <lklinker1@gmail.com>
2015-10-22 15:21:31 -0500

    change

--- src/classes/ViewMeetings.php
+++ src/classes/ViewMeetings.php
@@ -9,7 +9,7 @@ class ViewMeetings {
             $result = $stmt->execute();
 
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                echo "user_id: " . $row['user_id'] . ", conference room: " . $row['conference_room_id'] . "<br/>";
+                echo "<h3>user_id: " . $row['user_id'] . ", conference room: " . $row['conference_room_id'] . "</h3><br/>";
             }
         } catch(Exception $e) {
             echo $e->getMessage();
