a683a093534c913b329f579241d92dff997ee0e1
Luke Klinker <lklinker1@gmail.com>
2015-10-28 13:50:19 -0500

    fix

--- src/classes/ViewMeetings.php
+++ src/classes/ViewMeetings.php
@@ -24,19 +24,19 @@ class ViewMeetings {
             echo '              <th>Time</th>' . "\r\n";
             echo '      </tr>' . "\r\n";
             echo '  </thead>' . "\r\n";
-
-	    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             echo '  <tbody>' . "\r\n";
-            echo '      <tr>' . "\r\n";
-            echo '         <td class="mdl-data-table__cell--non-numeric"> ' . $row['last_name'] . '</td>' . "\r\n";
-	        echo '         <td>' . $row['name'] . '</td>' . "\r\n";
-	        echo '         <td>' . $row['room_number'] . '</td>' . "\r\n";
- 	        echo '         <td>' . $row['start_time'] . '</td>' . "\r\n";
- 	        echo '      <tr>' . "\r\n";
-        }
 
-        echo '  </tbody>' . "\r\n";
-        echo '</table>' . "\r\n";
+    	    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                echo '      <tr>' . "\r\n";
+                echo '         <td class="mdl-data-table__cell--non-numeric"> ' . $row['last_name'] . '</td>' . "\r\n";
+    	        echo '         <td>' . $row['name'] . '</td>' . "\r\n";
+    	        echo '         <td>' . $row['room_number'] . '</td>' . "\r\n";
+     	        echo '         <td>' . $row['start_time'] . '</td>' . "\r\n";
+     	        echo '      <tr>' . "\r\n";
+            }
+
+            echo '  </tbody>' . "\r\n";
+            echo '</table>' . "\r\n";
 
         } catch(Exception $e) {
             echo $e->getMessage();
