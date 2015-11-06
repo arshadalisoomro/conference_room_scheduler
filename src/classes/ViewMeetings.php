707208b9ea8ab02a73a3f4e83c8e50d20376cfc9
Luke Klinker <lklinker1@gmail.com>
2015-11-06 10:50:14 -0600

    add date to the view reservations table

--- src/classes/ViewMeetings.php
+++ src/classes/ViewMeetings.php
@@ -10,13 +10,13 @@ class ViewMeetings {
         echo "<h3>";
         if ($tableType == 'me') {
             echo "Future Reservations";
-            $query = "SELECT res._id AS _id, first_name, last_name, name, room_number, start_time, end_time FROM reservation res JOIN room r ON res.conference_room_id = r._id JOIN location l ON r.location_id = l._id JOIN user u ON res.user_id = u._id JOIN time_slot t ON res.time_slot_id = t._id WHERE date >= CURDATE() AND user_id = " . $userId;
+            $query = "SELECT res._id AS _id, first_name, last_name, name, room_number, start_time, end_time, date FROM reservation res JOIN room r ON res.conference_room_id = r._id JOIN location l ON r.location_id = l._id JOIN user u ON res.user_id = u._id JOIN time_slot t ON res.time_slot_id = t._id WHERE date >= CURDATE() AND user_id = " . $userId;
         } else if ($tableType == 'users') {
             echo "Created User's Reservations";
-            $query = "SELECT res._id AS _id, first_name, last_name, name, room_number, start_time, end_time FROM reservation res JOIN room r ON res.conference_room_id = r._id JOIN location l ON r.location_id = l._id JOIN user u ON res.user_id = u._id JOIN time_slot t ON res.time_slot_id = t._id WHERE date >= CURDATE() AND created_by_id = " . $userId;
+            $query = "SELECT res._id AS _id, first_name, last_name, name, room_number, start_time, end_time, date FROM reservation res JOIN room r ON res.conference_room_id = r._id JOIN location l ON r.location_id = l._id JOIN user u ON res.user_id = u._id JOIN time_slot t ON res.time_slot_id = t._id WHERE date >= CURDATE() AND created_by_id = " . $userId;
         } else { // all users
             echo "All User's Reservations";
-            $query = "SELECT res._id AS _id, first_name, last_name, name, room_number, start_time, end_time FROM reservation res JOIN room r ON res.conference_room_id = r._id JOIN location l ON r.location_id = l._id JOIN user u ON res.user_id = u._id JOIN time_slot t ON res.time_slot_id = t._id WHERE date >= CURDATE()";
+            $query = "SELECT res._id AS _id, first_name, last_name, name, room_number, start_time, end_time, date FROM reservation res JOIN room r ON res.conference_room_id = r._id JOIN location l ON r.location_id = l._id JOIN user u ON res.user_id = u._id JOIN time_slot t ON res.time_slot_id = t._id WHERE date >= CURDATE()";
         }
         echo "</h3>" . "\r\n";
 
@@ -36,6 +36,7 @@ class ViewMeetings {
             echo '          <th class="mdl-data-table__cell--non-numeric">Name</th>' . "\r\n";
             echo '              <th>Building</th>' . "\r\n";
             echo '              <th>Room Number</th>' . "\r\n";
+            echo '              <th>Date</th>' . "\r\n";
             echo '              <th>Start Time</th>' . "\r\n";
             echo '              <th>End Time</th>' . "\r\n";
             echo '              <th>Delete?</th>' . "\r\n";
@@ -48,6 +49,7 @@ class ViewMeetings {
                 echo '         <td>' . $row['first_name'] . ' ' . $row['last_name'] . '</td>' . "\r\n";
     	        echo '         <td>' . $row['name'] . '</td>' . "\r\n";
     	        echo '         <td>' . $row['room_number'] . '</td>' . "\r\n";
+                echo '         <td>' . $row['date'] . '</td>' . "\r\n";
      	        echo '         <td>' . $row['start_time'] . '</td>' . "\r\n";
                 echo '         <td>' . $row['end_time'] . '</td>' . "\r\n";
                 echo '         <td>' . "<a class='home_page_link' onclick='return confirm(\"Are you sure?\")'href='cancel_reservation.php?reservation_id=" . $row['_id']  . "'>Delete</a>" . '</td>' . "\r\n";
