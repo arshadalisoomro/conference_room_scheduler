daaa68662af4f94f3322fbeef2c5415ca768bd2c
Luke Klinker <lklinker1@gmail.com>
2015-11-06 13:51:25 -0600

    try inserting into the recurrence table

--- src/schedule_reservation.php
+++ src/schedule_reservation.php
@@ -5,20 +5,32 @@ AutoLoader::registerDirectory('../src/classes');
 
 require("config.php");
 
-$insertStatement = "INSERT INTO reservation 
-					(`user_id`, `conference_room_id`, `time_slot_id`, `date`) 
-					VALUES (:user_id,:conference_room_id,:time_slot_id,:date_val)";
+$insertStatement;
+$insertParams;
+if (empty($_GET['recurrence'])) {
+    $insertStatement = "INSERT INTO reservation 
+                    (`user_id`, `conference_room_id`, `time_slot_id`, `recurrence_id`, `date`) 
+                    VALUES (:user_id,:conference_room_id,:time_slot_id,:recurrence_id,:date_val)";
 
-$insertParams = array(
-            ':user_id' => $_GET['user_id'],
-            ':conference_room_id' => $_GET['room_id'],
-            ':time_slot_id' => $_GET['time_slot'],
-            ':date_val' => $_GET['date']
-        );
+    $insertParams = array(
+                ':user_id' => $_GET['user_id'],
+                ':conference_room_id' => $_GET['room_id'],
+                ':time_slot_id' => $_GET['time_slot'],
+                ':recurrence_id' => '0',
+                ':date_val' => $_GET['date']
+            );
+} else {
+    $createRecurrence = "INSERT INTO recurrence (`recurrence_type_id`) VALUES (:_id)";
+    $createParams = array(':_id' => $_GET['recurrence']);
+    $stmt = $db->prepare($createRecurrence);
+    $result = $stmt->execute($createParams);
+    print_r($result);
+}
+
 
 try {
-    $stmt = $db->prepare($insertStatement);
-    $result = $stmt->execute($insertParams);
+    //$stmt = $db->prepare($insertStatement);
+    //$result = $stmt->execute($insertParams);
 
 	//header("Location: home.php");
 	//die("Redirecting to home.php");
