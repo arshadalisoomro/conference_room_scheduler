251db3c79694bbae5596d42a8f7655c34a33b100
iowamao <haowen-xu@uiowa.edu>
2015-11-07 16:57:56 -0600

    re-styled_table

--- src/classes/ViewRoom.php
+++ src/classes/ViewRoom.php
@@ -28,7 +28,7 @@ class ViewRoom {
             // the .\r\n just creates
 
             echo '<table align="center" class="mdl-data-table mdl-js-data-table mdl-data-table mdl-shadow--2dp" style="display:block;height:400px;">' . "\r\n";
-            echo '  <thead >' . "\r\n";
+            echo '  <thead align="center">' . "\r\n";
             echo '      <tr >' . "\r\n";
             echo '          <th class="mdl-data-table__cell--non-numeric">Building</th>' . "\r\n";
             echo '              <th>Room</th>' . "\r\n";
@@ -38,7 +38,7 @@ class ViewRoom {
 			echo '              <th>Resource Types</th>' . "\r\n";
             echo '      </tr>' . "\r\n";
             echo '  </thead style="display:block;">' . "\r\n";
-            echo '  <tbody style="position:absolute; overflow: auto; height:100%; display:block;">' . "\r\n";
+            echo '  <tbody align="center" style="position:absolute; overflow: auto; height:100%; display:block;">' . "\r\n";
 
     	    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 								
@@ -70,7 +70,7 @@ class ViewRoom {
 			echo '<script type="text/javascript">';
 			echo 'function load_table_json(){ window["room_detail"]=json_encode('+$room_list_json+')';
 			echo 'console.log(window["room_detail"]); alert("test");}';
-			echo 'load_table_json()';
+			echo 'load_table_json();';
 			echo '</script>';
 			
 
