7f1c65b9d0ae033fbd827e34a08b1c517f8609ab
iowamao <haowen-xu@uiowa.edu>
2015-11-07 14:38:27 -0600

    Debug, the attribute name "quality_description" is typed wrong.

--- src/classes/ViewRoom.php
+++ src/classes/ViewRoom.php
@@ -10,7 +10,7 @@ class ViewRoom {
  $tableType = $post['type']; // will either be me, users, or all
 
         // you will have to join reservation with location, room, user, and time slot
-        $query="SELECT room.room_number,room.geometry,room.capacity,location.name,resource.description FROM room INNER JOIN location ON room.location_id=location._id INNER JOIN resource ON room._id=resource._id";
+        $query="SELECT room.room_number,room.geometry,room.capacity,location.name,resource.quality_description FROM room INNER JOIN location ON room.location_id=location._id INNER JOIN resource ON room._id=resource.room_id INNER JOIN resource_type ON resource.resource_type_id=resource_type._id ";
         echo "<h3>";
 		
 		echo "Room Details";
@@ -44,7 +44,7 @@ class ViewRoom {
     	        echo '         <td>' . $row['room_number'] . '</td>' . "\r\n";
                 echo '         <td>' . $row['capacity'] . '</td>' . "\r\n";
 				echo '         <td>' . $row['geometry'] . '</td>' . "\r\n";
-				echo '         <td>' . $row['description'] . '</td>' . "\r\n";
+				echo '         <td>' . $row['quality_description'] . '</td>' . "\r\n";
                 //echo '         <td>' . "<a class='home_page_link' onclick='return confirm(\"Are you sure?\")'href='cancel_reservation.php?reservation_id=" . $row['_id']  . "'>Delete</a>" . '</td>' . "\r\n";
      	        echo '      </tr>' . "\r\n";
             }
