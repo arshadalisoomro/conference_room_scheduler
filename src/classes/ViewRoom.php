c0f246ad1915aaf8a9ea3ece8871a09899ce7acf
iowamao <haowen-xu@uiowa.edu>
2015-11-07 17:49:59 -0600

    test21

--- src/classes/ViewRoom.php
+++ src/classes/ViewRoom.php
@@ -66,7 +66,7 @@ class ViewRoom {
 
             echo '  </tbody>' . "\r\n";
             echo '</table>' . "\r\n";
-			echo $room_list_json;
+			echo json_encode($room_list_json);
 			echo '<script type="text/javascript">console.log('+json_encode($room_list_json)+');</script>';
 	
 			//echo '<script type="text/javascript">window["room_detail"]='+json_encode($room_list_json)+'; console.log(window["room_detail"]);</script>';
