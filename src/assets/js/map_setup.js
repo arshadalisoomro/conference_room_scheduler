f6678fab67d18023de8e226c145957942705cbdd
iowamao <haowen-xu@uiowa.edu>
2015-11-11 02:42:22 -0600

    fix1

--- src/assets/js/map_setup.js
+++ src/assets/js/map_setup.js
@@ -1,5 +1,5 @@
 
-var map = L.map('map').setView([41.660, -91.541], 14.5);
+var map = L.map('map').setView([41.660, -91.541], 14);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
