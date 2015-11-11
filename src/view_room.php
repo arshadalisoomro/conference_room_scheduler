3500c534422316de3c79c5e7e46b8950f16e7afc
iowamao <haowen-xu@uiowa.edu>
2015-11-11 01:17:09 -0600

    relocate

--- src/view_room.php
+++ src/view_room.php
@@ -46,9 +46,10 @@ error_reporting(E_ALL);
         </div>
         <main class="mdl-layout__content">
             <br/>
+			<div id="map" style="position:absolute; width:100%; height:300px;"></div>
             <section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
               <div class="mdl-card mdl-cell mdl-cell--12-col">
-			  <div id="map" style="position:absolute; width:100%; height:300px;"></div>
+			  
                 <div class="mdl-card__supporting-text">
 				
 				<div id="content">
