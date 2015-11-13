965514b566748d2e5e20227ea6f11624790737da
Luke Klinker <lklinker1@gmail.com>
2015-11-13 14:41:59 -0600

    added a page to display the users that a manager created, or view all users if you are an admin

---
+++ src/classes/ViewUsers.php
@@ -0,0 +1,49 @@
+<?php
+
+class ViewMeetings {
+	function buildTable($db, $post, $userId) {
+        $tableType = $post['type']; // will either be me, users, or all
+
+        // you will have to join reservation with location, room, user, and time slot
+        $query;
+
+        echo "<h3>";
+        if ($tableType == 'users') { // manager functionality
+            echo "Created Users";
+            $query = "SELECT u._id AS _id, first_name, last_name, description, email FROM user u JOIN user_type ut ON u.user_type_id = ut._id WHERE created_by_id = " . $userId;
+        } else { // all users
+            echo "All Users";
+            $query = "SELECT u._id AS _id, first_name, last_name, description, email FROM user u JOIN user_type ut ON u.user_type_id = ut._id ORDER BY ut._id AND u._id <> " . $userId;
+        }
+        echo "</h3>" . "\r\n";
+
+        try {
+            $stmt = $db->prepare($query);
+            $result = $stmt->execute();
+
+            echo '<table class="mdl-data-table mdl-js-data-table mdl-data-table mdl-shadow--2dp">' . "\r\n";
+            echo '  <thead>' . "\r\n";
+            echo '      <tr>' . "\r\n";
+            echo '          <th class="mdl-data-table__cell--non-numeric">Type</th>' . "\r\n";
+            echo '          <th>Name</th>' . "\r\n";
+            echo '          <th>Email</th>' . "\r\n";
+            echo '      </tr>' . "\r\n";
+            echo '  </thead>' . "\r\n";
+            echo '  <tbody>' . "\r\n";
+
+    	    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                echo '      <tr>' . "\r\n";
+                echo '         <td>' . $row['description'] . '</td>' . "\r\n";
+                echo '         <td>' . "<a class='home_page_link' href='user_page.php?id=" . $row['_id']  . "'>" . $row['first_name'] . ' ' . $row['last_name'] . "</a>" . '</td>' . "\r\n";
+    	        echo '         <td>' . $row['email'] . '</td>' . "\r\n";
+     	        echo '      </tr>' . "\r\n";
+            }
+
+            echo '  </tbody>' . "\r\n";
+            echo '</table>' . "\r\n";
+
+        } catch(Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+}
