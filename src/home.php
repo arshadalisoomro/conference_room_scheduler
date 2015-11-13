28047da104cfcce1f7507dcfb25a2059521e83b9
Luke Klinker <lklinker1@gmail.com>
2015-11-13 15:16:40 -0600

    change a string

--- src/home.php
+++ src/home.php
@@ -94,7 +94,7 @@
                         } else if ($userType == "admin") {
                             echo "<a class='home_page_link' href='add_user_form.php?type=2'>Add a New Manager</a><br/><br/>";
                             
-                            echo "<a class='home_page_link' href='view_users.php?type=all'>View Created Users</a><br/>";
+                            echo "<a class='home_page_link' href='view_users.php?type=all'>View All Users</a><br/>";
                             echo "<a class='home_page_link' href='view_meetings.php?type=all'>View All Reservations</a><br/><br/>";
 
                             echo "<a class='home_page_link' href='search_rooms.php'>Schedule New Reservation</a><br/>";
