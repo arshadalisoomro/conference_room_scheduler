dec10a213f50db0aaff2a5117fd93fb7c8cbdefb
Luke Klinker <lklinker1@gmail.com>
2015-11-19 11:09:41 -0600

    add a page for monthly reports

---
+++ src/monthly_report.php
@@ -0,0 +1,55 @@
+<?php
+
+    include_once('../AutoLoader.php');
+    AutoLoader::registerDirectory('../src/classes');
+
+    require("config.php");
+
+    $report = new MonthlyReport();
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Conference Room</title>
+    <meta name="description" content="Conference room management system for Database Systems">
+    <meta name="author" content="Team 6">
+
+    <link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.2/material.indigo-pink.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="../main.css" rel="stylesheet" type="text/css">
+</head>
+
+
+<body class="mdl-demo mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <header class="mdl-layout__header mdl-layout__header--waterfall">
+            <div class="mdl-layout__header-row">
+                <span class="mdl-layout-title">View Reservations</span>
+            </div>
+        </header>
+        <div class="mdl-layout__drawer">
+            <span class="mdl-layout-title">Scheduler</span>
+            <nav class="mdl-navigation">
+                <?php AccountDropdownBuilder::buildDropdown($db, $_SESSION) ?>
+            </nav>
+        </div>
+        <main class="mdl-layout__content">
+            <br/>
+            <section class="section--center--larger mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
+              <div class="mdl-card mdl-cell mdl-cell--12-col">
+                <div class="mdl-card__supporting-text">
+                    <div id="content">
+                        <?php $report->build($db, $_GET) ?>
+                    </div>
+                </div>
+              </div>
+            </section>
+            <br/>
+        </main>
+    </div>
+    
+    <script src="https://storage.googleapis.com/code.getmdl.io/1.0.2/material.min.js"></script>
+</body>
+</html>
